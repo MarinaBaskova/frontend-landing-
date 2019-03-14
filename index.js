2873b6d65dab6e63574e34451c409c13dc42b182
Marina Baskova <marinabaskova24@gmail.com>
2019-03-14 11:57:35 -0700

    added buttons animation and adjust carousel view

--- index.js
+++ index.js
@@ -3,46 +3,85 @@ const navMenu = document.querySelector('#header')
 const bannerHeader = document.querySelector('.banner-header')
 const bannerText = document.querySelector('.banner-text')
 const bannerBtn = document.querySelector('.banner-btn')
-
-
 let tl = new TimelineLite();
 
-window.addEventListener('load', function(){
-  tl.from(navMenu, 0.5, {left:100, autoAlpha:0});
-  tl.from(bannerHeader, 0.5, {left:-100, autoAlpha:0, delay:0.25});
-  tl.from(bannerText, 0.5, {scale:.5, autoAlpha:0, delay:0.25});
-  tl.from(bannerBtn, 0.5, {scale:.5, autoAlpha:0, delay:0.25});
+window.addEventListener('load', function () {
+    tl.from(navMenu, 0.5, {
+        left: 100,
+        autoAlpha: 0
+    });
+    tl.from(bannerHeader, 0.5, {
+        left: -100,
+        autoAlpha: 0,
+        delay: 0.25
+    });
+    tl.from(bannerText, 0.5, {
+        scale: .5,
+        autoAlpha: 0,
+        delay: 0.25
+    });
+    tl.from(bannerBtn, 0.5, {
+        scale: .5,
+        autoAlpha: 0,
+        delay: 0.25
+    });
 })
 
 // Collection icon Animation
-
 let icons = document.querySelectorAll('.icon');
-
 let allIcons = Array.from(icons);
 
 
-allIcons.forEach(function(icon){
-    icon.addEventListener('mouseover', function(){
-        icon.style.transform= 'scale(1.3)'
+allIcons.forEach(function (icon) {
+    icon.addEventListener('mouseover', function () {
+        icon.style.transform = 'scale(1.3)'
     })
-    icon.addEventListener('mouseout', function(){
-        icon.style.transform= 'scale(1)'
+    icon.addEventListener('mouseout', function () {
+        icon.style.transform = 'scale(1)'
     })
 })
 
 
-//P ELEMENT ANIMATION
- let pTags = document.querySelectorAll('.stat-text');
- let allpTags = Array.from(pTags);
+// P Elements Animation
+let pTags = document.querySelectorAll('.stat-text');
+let allpTags = Array.from(pTags);
 
-allpTags.map( p => {
-  p.addEventListener('mouseover', p => {
-    TweenMax.to(p.currentTarget, 1, {scale: 1.1, ease:Bounce.easeOut})
-  })
+allpTags.map(p => {
+    p.addEventListener('mouseover', p => {
+        TweenMax.to(p.currentTarget, 1, {
+            scale: 1.1,
+            ease: Bounce.easeOut
+        })
+    })
 })
 
-allpTags.map( p => {
-  p.addEventListener('mouseout', p => {
-    TweenMax.to(p.currentTarget, 1, {scale: 1, ease:Bounce.easeOut})
-  })
+allpTags.map(p => {
+    p.addEventListener('mouseout', p => {
+        TweenMax.to(p.currentTarget, 1, {
+            scale: 1,
+            ease: Bounce.easeOut
+        })
+    })
+})
+
+// Buttons  Elements Animation
+let buttons = document.querySelectorAll('button');
+let allButtons = Array.from(buttons);
+
+allButtons.map(b => {
+    b.addEventListener('mouseover', b => {
+        TweenMax.to(b.currentTarget, 1, {
+            scale: 1.1,
+            ease: Bounce.easeOut
+        })
+    })
+})
+
+allButtons.map(b => {
+    b.addEventListener('mouseout', b => {
+        TweenMax.to(b.currentTarget, 1, {
+            scale: 1,
+            ease: Back.easeIn.config(1)
+        });
+    })
 })
